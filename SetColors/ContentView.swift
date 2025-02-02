f7d718a89b984a59d2ab9e5bd8a41080aacfca42
Yaroslav Malygin <ya.malygin@team.superjob.ru>
2025-02-02 11:59:23 +0300

    Initial Commit

---
+++ SetColors/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SetColors
+//
+//  Created by Yaroslav Malygin on 02.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SetColors/SetColorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetColorsApp.swift
+//  SetColors
+//
+//  Created by Yaroslav Malygin on 02.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct SetColorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
